58d02d856fdbd0eed2ffd86c497f9511cf686800
Ola <o.dominas@gmail.com>
2018-12-05 00:48:16 +0100

    More stars

--- Assets/PositionObjects.cs
+++ Assets/PositionObjects.cs
@@ -14,27 +14,27 @@ public class PositionObjects : MonoBehaviour
 
 		float radius = 20;
 		float theta = Random.Range(0, 360) * Mathf.Deg2Rad;
-		float phi = Random.Range(0, 360) * Mathf.Deg2Rad;
+		float phi = Random.Range(0, 180) * Mathf.Deg2Rad;
 
 		float x = radius * Mathf.Cos(theta) * Mathf.Sin(phi);
 		float y = radius * Mathf.Cos(phi);
-		float z = radius * Mathf.Sin(theta) * Mathf.Cos(phi);
+		float z = radius * Mathf.Sin(theta) * Mathf.Sin(phi);
 
 		star.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
 
 		star.transform.position = new Vector3(x, y, z);
 
-		GameObject[] lights = new GameObject[100];
+		GameObject[] lights = new GameObject[250];
 		
-		for (int i = 0; i < 100; i++)
+		for (int i = 0; i < 250; i++)
 		{
 			float radius1 = 20;
 			float theta1 = Random.Range(0, 360) * Mathf.Deg2Rad;
-			float phi1 = Random.Range(0, 360) * Mathf.Deg2Rad;
+			float phi1 = Random.Range(0, 180) * Mathf.Deg2Rad;
 
 			float x1 = radius1 * Mathf.Cos(theta1) * Mathf.Sin(phi1);
 			float y1 = radius1 * Mathf.Cos(phi1);
-			float z1 = radius1 * Mathf.Sin(theta1) * Mathf.Cos(phi1);
+			float z1 = radius1 * Mathf.Sin(theta1) * Mathf.Sin(phi1);
 
 			GameObject obj = Instantiate(star, new Vector3(x1, y1, z1), Quaternion.identity);
 
@@ -43,10 +43,7 @@ public class PositionObjects : MonoBehaviour
 			obj.gameObject.tag = "Star";
 
 			lights[i] = obj;
-
 		}
-
-
 	}
 	// Use this for initialization
 	void Start()
